8af9826aed765cf7b1f026c4e46fb5c2ce722698
taeyoung <xodud5080@naver.com>
2024-02-06 10:32:24 +0900

    수정 후

--- src/day02/Quiz1.java
+++ src/day02/Quiz1.java
@@ -3,14 +3,10 @@ package day02;
 public class Quiz1 {
 	public static void main(String[] args) {
 		String name = "김말이";
-		int age = 20;
+		int age = 20, iq = 120;
 		double height = 173.3;
-		int iq = 120;
 		char grade = 'B';
-		System.out.println("이름\t: " + name);
-		System.out.println("나이\t: " + age);
-		System.out.println("키\t: " + height);
-		System.out.println("아이큐\t: " + iq);
-		System.out.println("등급\t: " + grade);
+		System.out.println("이름\t: "+name+"\n나이\t: "+age+"\n키\t: "+height+"\n아이큐\t: "+iq+"\n등급\t: "+grade);
+	
 	}
 }
